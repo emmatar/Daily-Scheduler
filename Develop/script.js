f0f713f3c416b682dcb415094eec3e12c063ef5f
Elijah Matar <m.matar515@gmail.com>
2024-01-24 15:06:00 -0600

    successfully edited script file to change color of the textareas in correlation to time of day. yay!

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,92 @@
   // TODO: Add code to display the current date in the header of the page.
 function displayTime () {
-  var today = dayjs();
-  $('#currentDay').text(today.format('dddd, MMMM D h:mm A'))
+  var todayTimeHeader = dayjs();
+  $('#currentDay').text(todayTimeHeader.format('dddd, MMMM D h:mm:ss A'))
 }
 
-var number = 9;
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
   var template = document.querySelector("#time-block-template");
   // for-loop to rotate through hours of workday
+  var today = dayjs().format()
+  var number = 9;
   for(var i = 0; i < 9; i++) {
-    var today = dayjs().hour()
-    var hour = dayjs(today).format('h')
     var timeBlock = template.content.cloneNode(true);
     // select save button
     var $saveButton = $(timeBlock).find("button");
     // select DOM element with "hour of day" time and add one onto the time each loop
     var $timeNumber = $(timeBlock).find('#hour');
     var textBox = $(timeBlock).find("textarea")
+    textBox.text("Hey, You Got This.");
     if (number >= 9 && number < 12) {
+      var hourMilitary = parseInt(dayjs(today).format('H'))
+      if (number > hourMilitary) {
+        textBox.addClass('future');
+        textBox.removeClass('past');
+        textBox.removeClass('present');
+      } else if (number === hourMilitary) {
+        textBox.addClass('present');
+        textBox.removeClass('past');
+        textBox.removeClass('future');
+      } else {
+        textBox.addClass('past');
+        textBox.removeClass('future');
+        textBox.removeClass('present');
+      }
       $timeNumber.text(number + "AM")
       number++;
     } else if (number === 12) {
+      if (number > hourMilitary) {
+        textBox.addClass('future');
+        textBox.removeClass('past');
+        textBox.removeClass('present');
+      } else if (number === hourMilitary) {
+        textBox.addClass('present');
+        textBox.removeClass('past');
+        textBox.removeClass('future');
+      } else {
+        textBox.addClass('past');
+        textBox.removeClass('future');
+        textBox.removeClass('present');
+      }
       $timeNumber.text(number + "PM")
       number = 1;
     } else if (number >= 1 && number <= 5) {
+      var hourStandard = parseInt(dayjs(today).format('h'))
+      if (number > hourStandard) {      
+      textBox.addClass('future');
+      textBox.removeClass('past');
+      textBox.removeClass('present');
+    } else if (number === hourStandard) {
+      textBox.addClass('present');
+      textBox.removeClass('past');
+      textBox.removeClass('future');
+    } else {
+      textBox.addClass('past');
+      textBox.removeClass('future');
+      textBox.removeClass('present');
+    }
       $timeNumber.text(number + "PM")
       number++;
     } else {
       return;
     }
-    if(hour < number) {
-      textBox.addClass('past');
-      textBox.removeClass('future');
-      textBox.removeClass('present');
-    } else if(number === hour) {
-      textBox.addClass('past');
-      textBox.removeClass('future');
-      textBox.removeClass('present');
-    } else {
-      textBox.addClass('past');
-      textBox.removeClass('future');
-      textBox.removeClass('present');
-    }
+    // if(hour < number) {
+    //   textBox.addClass('past');
+    //   textBox.removeClass('future');
+    //   textBox.removeClass('present');
+    // } else if(number === hour) {
+    //   textBox.addClass('past');
+    //   textBox.removeClass('future');
+    //   textBox.removeClass('present');
+    // } else {
+    //   textBox.addClass('past');
+    //   textBox.removeClass('future');
+    //   textBox.removeClass('present');
+    // }
     $saveButton.attr("data-block", $timeNumber.text())
     $("#time-block-container").append(timeBlock);
   } 
